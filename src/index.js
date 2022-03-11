ead9cca7b8b96d825db2e278dbade4b44e1c2b65
Svetlana <lanavladi1605gmail.com>
2022-03-11 20:01:51 +0500

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    // let n = 123;
+    let arr = Math.abs(n).toString().split('').reverse().join(''); 
+  let result = Number(arr);
+  return result;
+//   console.log(typeof result);
 }
